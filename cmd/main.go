40b6e6958303d0a881609cb8ee10920b11cfc898
Dmitry Kuznetsov <dimaaash@gmail.com>
2024-05-08 20:58:09 -0400

    updating

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/dimaaash/bnc-test-1/infrastructure/db"
+
+	"github.com/dimaaash/bnc-test-1/usecases"
+
+	"github.com/dimaaash/bnc-test-1/interface/controllers"
+
+	"github.com/dimaaash/bnc-test-1/infrastructure/router"
+	"github.com/dimaaash/bnc-test-1/interface/repository"
+)
+
+var (
+	httpRouter router.Router = router.NewMuxRouter()
+	dbHandler  db.DBHandler
+)
+
+func getCardController() controllers.CardController {
+	cardRepo := repository.NewCardRepo(dbHandler)
+	cardInteractor := usecases.NewCardInteractor(cardRepo)
+	cardController := controllers.NewCardController(cardInteractor)
+	return *cardController
+}
+
+func getAccountController() controllers.AccountController {
+	accountRepo := repository.NewAccountRepo(dbHandler)
+	accountInteractor := usecases.NewAuthorInteractor(accountRepo)
+	accountController := controllers.NewAuthorController(accountInteractor)
+	return *accountController
+}
 
 func main() {
+
+	httpRouter.GET("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "App is up and running..")
+	})
+	var err error
+	dbHandler, err = db.NewDBHandler("mongodb://localhost:27017", "bookstore")
+	if err != nil {
+		log.Println("Unable to connect to the DataBase")
+		return
+	}
+	cardController := getCardController()
+	accountController := getAccountController()
+	httpRouter.POST("/book/add", cardController.Add)
+	httpRouter.GET("/book", accountController.FindAll)
+	httpRouter.POST("/author/add", accountController.Add)
+	httpRouter.SERVE(":8000")
 
 	fmt.Println("hello")
 }
